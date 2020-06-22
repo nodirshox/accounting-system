2861d96bf36c6bf712a76e15e7a821f056cb03eb
nodirshox <n.ergashev@student.inha.uz>
2020-06-22 15:17:25 +0500

    Improved UI Sign in/up

--- warehouse/models.py
+++ warehouse/models.py
@@ -2,16 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 
-# Client
-class Client(models.Model):
-    name = models.CharField(max_length=128, null=True)
-    number = models.CharField(max_length=8, null=True)
-    owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
 # Warehouse
 class Warehouse(models.Model):
     bonus = models.IntegerField(default=0, null=True)
@@ -24,8 +14,22 @@ class Warehouse(models.Model):
     def __str__(self):
         return self.owner.first_name
 
+# Client
+class Client(models.Model):
+    name = models.CharField(max_length=128, null=True)
+    number = models.CharField(max_length=8, null=True)
+    owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    warehouse = models.ForeignKey(Warehouse, null=True, on_delete=models.SET_NULL)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 class Recourse(models.Model):
     quantity = models.IntegerField()
     warehouse = models.ForeignKey(Warehouse, null=True, on_delete=models.SET_NULL)
     product = models.ForeignKey(Product, null=True, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return  self.product.name + ', ' + str(self.quantity)
