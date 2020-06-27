0760da2114cbcde3177b290b04dd50b4f4b090a7
nodirshox <n.ergashev@student.inha.uz>
2020-06-27 10:03:32 +0500

    Added new UI

--- account/decorators.py
+++ account/decorators.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 def unaunthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('dashbaord')
+            return redirect('dashboard')
         else:
             return view_func(request, *args, **kwargs)
 
