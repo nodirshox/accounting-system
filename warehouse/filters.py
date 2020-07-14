1223252525e5af3c7924d130295d923fbdec5014
nodirshox <n.ergashev@student.inha.uz>
2020-07-14 16:31:34 +0500

    Minor changes

--- warehouse/filters.py
+++ warehouse/filters.py
@@ -1,10 +1,11 @@
 import django_filters
+from django import forms
 from django_filters import CharFilter
 from .models import *
 
 class ClientFilter(django_filters.FilterSet):
-    name = CharFilter(field_name='name', lookup_expr='icontains', label="Name")    
-    number = CharFilter(field_name='number', lookup_expr='icontains', label='Number')
+    name = CharFilter(field_name='name', lookup_expr='icontains', label="", widget=forms.TextInput(attrs={'autocomplete': 'off', 'class': 'form-control float-right', 'placeholder': 'Name'}))    
+    number = CharFilter(field_name='number', lookup_expr='icontains', label='', widget=forms.TextInput(attrs={'autocomplete': 'off', 'class': 'form-control float-right', 'placeholder': 'Number'}))
     class Meta:
         model = Client
         fields = '__all__'
